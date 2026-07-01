3a5b8486bafba96b07a463dfb367c6e0a861cf3e
AmaanTheDeveloper <raoamaana@gmail.com>
2026-07-01 17:39:41 +0500

    Add Google authentication using Laravel Socialite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,13 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    
+     <a   href="{{route('googlelogin')}}"
+                            class="inline-block px-5 py-1.5 border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] rounded-sm text-sm leading-normal"
+                        >
+                            Continue with google
+                        </a>
+    
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Str;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,4 +17,33 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+// GOOGLE SOCIALITE LOGIN ROUTES
+Route::get("/auth/google",function(){
+
+return Socialite::driver('google')->redirect();
+})->name('googlelogin');
+
+Route::get("/auth/google/callback",function(){
+    try{
+$googleuser = Socialite::driver('google')->stateless()->user();
+
+$user=\App\Models\User::FirstOrCreate([
+'email'=>$googleuser->email,
+],
+[
+ 'name'=>$googleuser->name,
+ 'password'=>bcrypt(Str::random(16)),
+]);
+
+Auth::login($user);
+
+return redirect()->route('dashboard');
+}
+catch (\Exception $e) {
+        // Agar Google login mein koi error aaye toh wapis bhej dein
+        return redirect('/')->with('error', 'Google login nakam raha.');
+    }
+
 });
